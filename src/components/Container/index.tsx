c9da4a0872a01b796934fde15292b1ef6b6a9410
vitor-carmo <vitorv0071@gmail.com>
2022-07-30 16:37:16 -0300

    refactor: add scroll view a style

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { ScrollView } from "react-native";
 
-import { LinearGradient } from "./styles";
+import { ScrollView, LinearGradient } from "./styles";
 
 interface IContainerProps {
   children: React.ReactNode;
