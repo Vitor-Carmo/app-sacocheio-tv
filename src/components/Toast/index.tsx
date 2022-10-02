82945ecfca6c5d589c41af2db4d4771d87238016
vitor-carmo <vitorv0071@gmail.com>
2022-10-02 02:28:05 -0300

    refactor: adding new notification types and refactor them

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,7 +1,4 @@
-import RNToast, {
-  ToastConfig,
-  ToastProps,
-} from "react-native-toast-message";
+import RNToast, { ToastConfig, ToastProps } from "react-native-toast-message";
 import { useTheme } from "styled-components";
 
 import { BaseToast, BaseToastProps } from "./styles";
@@ -9,24 +6,39 @@ import { BaseToast, BaseToastProps } from "./styles";
 export default function Toast(props: ToastProps) {
   const { COLORS } = useTheme();
 
+  const BaseToastConfig = ({ color, ...props }: BaseToastProps) => (
+    <BaseToast
+      color={color}
+      text2NumberOfLines={3}
+      style={{
+        paddingVertical: 10,
+        height: "auto",
+      }}
+      text1Style={{
+        fontFamily: "Poppins_600SemiBold",
+        color: COLORS.TEXT,
+        fontSize: 14,
+        fontWeight: "600",
+      }}
+      text2Style={{
+        fontFamily: "Roboto_400Regular",
+        color: COLORS.TEXT_70,
+        fontSize: 12,
+        fontWeight: "400",
+      }}
+      {...props}
+    />
+  );
+
   const toastConfig: ToastConfig = {
     info: (props: BaseToastProps) => (
-      <BaseToast
-        color="#055EEB"
-        text1Style={{
-          fontFamily: "Poppins_600SemiBold",
-          color: COLORS.TEXT,
-          fontSize: 14,
-          fontWeight: "600",
-        }}
-        text2Style={{
-          fontFamily: "Roboto_400Regular",
-          color: COLORS.TEXT_70,
-          fontSize: 12,
-          fontWeight: "400",
-        }}
-        {...props}
-      />
+      <BaseToastConfig color="#055EEB" {...props} />
+    ),
+    error: (props: BaseToastProps) => (
+      <BaseToastConfig color="#E50606" {...props} />
+    ),
+    success: (props: BaseToastProps) => (
+      <BaseToastConfig color="#06E580" {...props} />
     ),
   };
   return <RNToast config={toastConfig} {...props} />;
