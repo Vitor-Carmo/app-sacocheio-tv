9e17c194b960bd0495333fdbd9b04d3cbcacd6db
vitor-carmo <vitorv0071@gmail.com>
2022-10-02 02:25:20 -0300

    feat: errors constants

---
+++ src/constants/errors.ts
@@ -0,0 +1,7 @@
+export default {
+  UNKNOWN_AUTHENTICATION_ERROR:
+    "Ocorreu algum erro ao tentar autenticar o usuário. Por favor, tente mais tarde!",
+  AUTHENTICATION_ERROR:
+    "Houve uma falha ao tentar autenticar. Verifique se o e-mail ou a senha estão corretos.",
+  INVALID_EMAIL_ERROR: "E-mail inválido!",
+};
