88926e3c9094eeca8bf10aae89db59569eb8066f
vitor-carmo <vitorv0071@gmail.com>
2022-10-23 23:03:52 -0300

    loading podcast

---
+++ src/components/Loading/LoadingPodcast/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import {
+  Container,
+  AvatarContainer,
+
+} from "../../Podcast/styles";
+
+
+import ShimmerPlaceholder from "../../ShimmerPlaceholder";
+
+export type PodcastProps = {
+  isLastPodcast?: boolean;
+};
+
+export default function LoadingPodcast({ isLastPodcast }: PodcastProps) {
+
+  return (
+    <Container isLastPodcast={isLastPodcast} >
+      <AvatarContainer>
+        <ShimmerPlaceholder
+          borderRadius={5}
+          width={60}
+          height={60}
+          marginRight={10}
+        />
+        <ShimmerPlaceholder borderRadius={2} width="50%" height={16} />
+      </AvatarContainer>
+      <ShimmerPlaceholder
+        borderRadius={2}
+        width="100%"
+        height={12}
+        marginBottom={5}
+      />
+      <ShimmerPlaceholder
+        borderRadius={2}
+        width="90%"
+        height={12}
+        marginBottom={12}
+      />
+
+    </Container>
+  );
+}
--- src/components/Loading/index.ts
+++ src/components/Loading/index.ts
@@ -3,5 +3,13 @@ import LatestPodcasts from "./LatestPodcasts";
 import FeaturedPodcast from "./FeaturedPodcast";
 import LoadingLikedEpisodes from "./LikedEpisodes";
 import LoadingPrograms from "./LoadingPrograms";
+import LoadingPodcast from "./LoadingPodcast";
 
-export { LoadingShows, LatestPodcasts, FeaturedPodcast, LoadingLikedEpisodes, LoadingPrograms };
+export {
+  LoadingShows,
+  LatestPodcasts,
+  FeaturedPodcast,
+  LoadingLikedEpisodes,
+  LoadingPrograms,
+  LoadingPodcast,
+};
