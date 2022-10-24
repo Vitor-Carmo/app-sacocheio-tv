620f8c89a5fdbc1226b47dd3d29b35be3b3ed1ef
vitor-carmo <vitorv0071@gmail.com>
2022-10-23 22:56:43 -0300

    adding podcast color

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,5 +2,6 @@ import greetings from "./greetings";
 import HexToHSL from "./HexToHSL";
 import podcastIcon from "./podcastIcon";
 import formateDate from "./formateDate";
+import podcastColor from "./podcastColor";
 
-export { greetings, HexToHSL, podcastIcon, formateDate };
+export { greetings, HexToHSL, podcastIcon, formateDate, podcastColor };
