3306a0bd508b9e5e1cd09c5122f6f59314c0170f
vitor-carmo <vitorv0071@gmail.com>
2022-10-23 22:53:56 -0300

    feat: SeeMore component

---
+++ src/components/SeeMore/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const ShowMoreOrLessText = styled.Text`
+  font-family: "Poppins_600SemiBold";
+  font-size: 13px;
+  margin-top: 0px;
+  color: ${({ theme }) => theme.COLORS.TEXT};
+`;
