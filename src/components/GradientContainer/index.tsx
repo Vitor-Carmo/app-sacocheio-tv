55ab7317ff2b2d00de36d9a604aa5c33158341eb
vitor-carmo <vitorv0071@gmail.com>
2022-10-23 23:00:34 -0300

    pass backgroundColorGradientCount to GradientContainer

--- src/components/GradientContainer/index.tsx
+++ src/components/GradientContainer/index.tsx
@@ -1,5 +1,6 @@
-import React, { ReactNode, useEffect } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { ViewProps } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 import { useTheme } from "styled-components";
 
 import { HexToHSL } from "../../helpers";
@@ -10,23 +11,28 @@ interface GradientContainerProps extends ViewProps {
   children?: ReactNode;
   color?: string;
   opacity?: number;
+  backgroundColorGradientCount?: number;
 }
 
 export default function GradientContainer({
   children,
   color = "#806E42",
   opacity = 1,
+  backgroundColorGradientCount = 3,
   ...props
 }: GradientContainerProps) {
   const { COLORS, DIMENSIONS } = useTheme();
+
+  console.log();
 
   return (
     <Container
       colors={[
         HexToHSL(color, DIMENSIONS.HSL_LIGHTNESS, opacity),
-        COLORS.BACKGROUND,
-        COLORS.BACKGROUND,
-        COLORS.BACKGROUND,
+        ...`${COLORS.BACKGROUND} `
+          .repeat(backgroundColorGradientCount - 1 > 0 ? backgroundColorGradientCount - 1 : 1)
+          .trim()
+          .split(" "),
       ]}
       {...props}
     >
