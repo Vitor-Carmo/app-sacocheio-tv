af8f143dcdb1942f8168bf2eb41d018c5358365f
vitor-carmo <vitorv0071@gmail.com>
2022-10-27 21:41:33 -0300

    feat: liked episodes screen

--- src/screens/Likes/index.tsx
+++ src/screens/Likes/index.tsx
@@ -1,18 +1,61 @@
-import React from "react";
-import Podcast from "../../components/Podcast";
+import React, { useEffect, useState } from "react";
+import { Podcast, Loading } from "../../components";
+import api from "../../services/api";
 
-import { PodcastList } from "./styles";
+import { Container } from "./styles";
 
 export default function Likes() {
-  const podcasts = [{}, {}];
+  const [podcasts, setPodcasts] = useState<IFavoriteEpisode[]>([]);
+  const [loading, setLoading] = useState(true);
+  const { LoadingPodcast } = Loading;
+
+  useEffect(() => {
+    const handleFetchLikedEpisodes = async () => {
+      setLoading(true);
+      try {
+        const {
+          data: { data },
+        } = await api.get<FavoritesEpisodeResponse>("/podcast/favorites");
+        setPodcasts(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    handleFetchLikedEpisodes();
+  }, []);
 
   return (
-    <PodcastList
-      data={podcasts}
-      renderItem={({ item, index }) => (
-        <Podcast isLastPodcast={podcasts.length - 1 === index} />
+    <Container>
+      {!loading ? (
+        <>
+          {podcasts.map((podcast, index) => (
+            <Podcast
+              key={podcast.episode.id}
+              id={podcast.episode.id}
+              podcastId={podcast.id}
+              titulo={podcast.episode.titulo}
+              data={podcast.episode.data}
+              descricao={podcast.episode.descricao}
+              isFavorite={podcast.episode.isFavorite}
+              slug={podcast.episode.slug}
+              podcastName={podcast.episode.podcastName}
+              thumbnail={podcast.episode.thumbnail}
+              podcastUrl={podcast.url}
+              isLastPodcast={podcasts.length - 1 === index}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <LoadingPodcast />
+          <LoadingPodcast />
+          <LoadingPodcast />
+          <LoadingPodcast isLastPodcast />
+        </>
       )}
-      keyExtractor={(item, index) => "key" + index}
-    />
+    </Container>
   );
 }
--- src/screens/Likes/styles.ts
+++ src/screens/Likes/styles.ts
@@ -1,6 +1,11 @@
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
 
 export const PodcastList = styled.FlatList`
+  flex: 1;
+  padding-bottom: ${({ theme }) => theme.DIMENSIONS.MARGIN_BOTTOM_TO_TAB_BAR};
+`;
+export const Container = styled.ScrollView`
   flex: 1;
   padding-bottom: ${({ theme }) => theme.DIMENSIONS.MARGIN_BOTTOM_TO_TAB_BAR};
 `;
