b8dd4796489f0ef19034e0bf74c53c3d6d035d62
vitor-carmo <vitorv0071@gmail.com>
2022-11-13 19:43:47 -0300

    change api to local

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -3,5 +3,5 @@ export default {
   SACOCHEIO_TV_PORTAL: "https://portal.sacocheio.tv",
   SPOTIFY_PLAYLIST: "https://open.spotify.com/playlist/3aEIIBiCY3mwEjf2juG6f7",
   FORGOT_PASSWORD: `https://www.sacocheio.tv/recuperarSenha`,
-  MIDDLEWARE_API: `https://api-sacocheio-tv.000webhostapp.com`,
+  MIDDLEWARE_API: `http://192.168.122.1/api-sacocheio-tv`,
 };
