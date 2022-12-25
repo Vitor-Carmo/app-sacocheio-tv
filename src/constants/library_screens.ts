e9689b667252f04d94386c1b5da30612bc1fd8c6
vitor-carmo <vitorv0071@gmail.com>
2022-12-25 10:55:33 -0300

    change 'biblioteca' screens order

--- src/constants/library_screens.ts
+++ src/constants/library_screens.ts
@@ -3,19 +3,19 @@ import Likes from "../screens/Likes";
 
 export default [
   {
-    title: "Baixados",
-    filterLabel: "Todos os episódios baixados",
-    screen: {
-      name: "Downloads",
-      component: Downloads,
-    },
-  },
-  {
     title: "Curtidas",
     filterLabel: "Todos os episódios curtidos",
     screen: {
       name: "Likes",
       component: Likes,
+    },
+  },
+  {
+    title: "Baixados",
+    filterLabel: "Todos os episódios baixados",
+    screen: {
+      name: "Downloads",
+      component: Downloads,
     },
   },
 ];
